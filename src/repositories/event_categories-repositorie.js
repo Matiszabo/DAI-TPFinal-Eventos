9022d4cc362ec4bb00c064e8ae899841a55de330
Android Studio <devandroid@ort>
2024-05-13 11:55:27 -0300

    avanzado

--- src/repositories/event_categories-repositorie.js
+++ src/repositories/event_categories-repositorie.js
@@ -1,63 +1,116 @@
-import DBConfig from './../configs/dbConfig.js';
+import DBConfig from '../configs/db-config.js';
 import pkg from 'pg'
-const { Client, Pool }  = pkg;
+const {Client, Pool} = pkg;
 
-export default class event_categories {
-    getAllAsync = async () => {
+export default class CategoriesRepository{
+    getAllAsync = async() => {
+        console.log(CategoriesRepository.createAsync());
         let returnArray = null;
         const client = new Client(DBConfig);
-        try {
-                await client.connect();
-                const sql = `SELECT * FROM event_categories`;
-                const result = await client.query(sql);
-                await client.end();
+        try{
+            await client.connect();
+            const sql = `SELECT * FROM event_categories`;
+            const result = await client.query(sql);
+            await client.end();
             returnArray = result.rows;
         } catch (error) {
             console.log(error);
         }
         return returnArray;
     }
+
+
     getByIdAsync = async (id) => {
-        const sql = 'SELECT * from event_categories WHERE id=$1'; 
-        const values = [id]; 
-        const result = await 
-        client.query(sql, values); await client.end(); 
-        console.log('Resultados: length', result.rows.length); 
-        console.log('result.rows[0]', result.rows[0]);
-    }
-    createAsync = async (entity) => {
-        const sql = `INSERT INTO event_categories (name, display_order)
-        VALUES ($1, $2)`;
-        const values = ['futbol', 100];
-        const result = await client.query(sql, values); 
-        await client.end();
-        console.log('rowCount: ', result.rowCount);
-    }
-    updateAsync = async (entity) => {
-        console.log(`event_categories.updateAsync(${JSON.stringify(entity)})`);
-        let 
-        const sql = `UPDATE event_category
-        SET  id = 34, name = Provincia Modificada, display_order = 100
-        WHERE id = $1 `;
-        const values = [ 
-        id, //1
-        emntity.name ?? previousEntity?.name,
-        emntity.display_order ?? previousEntity?.display_order,
-        ];
-        const result = await client.query(sql, values);
-    }
-    deleteByIdAsync = async (id) => {
-        console.log(`event_categories.deleteByIdAsync(${id})`);
-        let rowsAffected = 0;
+        console.log(`CategoriesRepository.getByIdAsync(${id})`);
+        let returnEntity = null;
+        const client = new Client(DBConfig);
         try{
-            const client = await this.getDBClient();
-            const sql=`DELETE FROM provinces WHERE id = $1`;
+            await client.connect();
+            const sql = `SELECT * FROM event_categories WHERE id=$1`;
             const values = [id];
             const result = await client.query(sql, values);
+            await client.end();
+            if(result.rows.length > 0){
+                returnEntity = result.rows[0];
+            }
+        } catch (error) {
+            console.log(error);
+        }
+        console.log("returnEntity", returnEntity)
+        return returnEntity;
+    }
+
+
+    createAsync = async (entity) => {
+        console.log(`CategoriesRepository.createAsync(${JSON.stringify(entity)})`); 
+        let rowsAffected = 0;
+        const client = new Client(DBConfig);
+        try{
+            await client.connect();
+            const sql = `Insert INTO event_categories (
+                    name            ,
+                    display_order   
+                )  VALUES (
+                    $1,
+                    $2,     
+                )`;
+            const values = [    entity?.name            ?? '',
+                                entity?.display_order   ?? 0
+                           ];
+            const result = await client.query(sql, values);
+            await client.end();
             rowsAffected = result.rowCount;
-        }catch(error) {
+        } catch (error) {
             console.log(error);
         }
         return rowsAffected;
     }
+    updateAsync = async (entity) => {
+        console.log(`CategoriesRepository.updateAsync(${JSON.stringify(entity)})`); 
+        let rowsAffected = 0;
+        let id = entity.id 
+        console.log('REPO entity', entity)
+        const client = new Client(DBConfig);
+        try{
+            const previousEntity = await this.getByIdAsync(id);
+            console.log('previousEntity', previousEntity)
+            if(previousEntity== null) return 0;
+            
+            await client.connect();
+
+            const sql=`UPDATE event_categories SET
+                name =  $2        ,
+                display_order = $3
+                WHERE id = $1`;  
+        const values = [    id,
+                            entity?.name            ?? previousEntity?.name,
+                            entity?.display_order   ?? previousEntity?.display_order
+                       ]; 
+        const result = await client.query(sql, values);
+         await client.end();
+         rowsAffected = result.rowCount;
+    } catch (error) {
+        LogHelper.logError(error);
+        console.log(error);
+    }
+     return rowsAffected;
+  }
+
+    deleteByIdAsync = async (id) => {
+        console.log(`CategoriesRepository.deleteByIdAsync(${id})`);
+        let rowsAffected = 0;
+        const client = new Client(DBConfig);
+        try{
+            await client.connect();
+            const sql = `DELETE FROM event_categories WHERE id=$1`;
+            const values = [id];
+            const result = await client.query(sql, values);
+            await client.end();
+          rowsAffected = result.rowCount;
+        } catch (error) {
+            console.log(error);
+        }
+        return rowsAffected;
+    }
+
 }
